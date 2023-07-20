35f19ea71085f8a74b9f863bce242699848a78b6
AshKiano <ash.kiano@gmail.com>
2023-07-20 20:09:58 +0200

    version 1.4

--- src/main/java/com/ashkiano/icarus/Icarus.java
+++ src/main/java/com/ashkiano/icarus/Icarus.java
@@ -9,7 +9,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 //TODO udělat překlady
 //TODO configurovat výšky hlášek
-//TODO udelat permisi configurovatelnou
 //inspired by https://www.spigotmc.org/resources/icarus.62287/
 
 // The main class of the plugin, implementing the Listener interface to listen to events
@@ -19,6 +18,8 @@ public class Icarus extends JavaPlugin implements Listener {
     private int fireDuration;
     private String fireMessage;
     private boolean shouldDisplayMessage;
+    private int messageHeight;
+    private String message;
     private String bypassPermission; // This will hold the bypass permission set in the config file
 
     // Called when the plugin is enabled
@@ -33,6 +34,8 @@ public class Icarus extends JavaPlugin implements Listener {
         fireDuration = config.getInt("fire-duration");
         fireMessage = config.getString("fire-message");
         shouldDisplayMessage = config.getBoolean("display-message");
+        messageHeight = config.getInt("message-height");
+        message = config.getString("message");
         // Load the bypass permission from the config file, defaulting to "icarus.bypass" if it's not there
         bypassPermission = config.getString("bypass-permission", "icarus.bypass");
         // Register the event listener
@@ -53,6 +56,10 @@ public class Icarus extends JavaPlugin implements Listener {
             player.setFireTicks(20 * fireDuration);
             // If shouldDisplayMessage is true, send the player a message
             if (shouldDisplayMessage) player.sendMessage(fireMessage);
+        }
+        // If the player's Y position is greater than messageHeight, send them a warning
+        else if (player.getLocation().getBlockY() > messageHeight) {
+            player.sendMessage(message);
         }
     }
 }
